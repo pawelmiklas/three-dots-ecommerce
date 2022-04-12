378faeb841d7832c0908e107f015ddbc5111469d
Paweł Mikłas <pablomiklas@gmail.com>
2022-04-12 21:51:39 +0200

    Change path

--- components/Layout/Nav/MidBar.tsx
+++ components/Layout/Nav/MidBar.tsx
@@ -1,31 +1,37 @@
-import { Row, Col} from 'antd';
-import Image from 'next/image';
-import router from 'next/router';
-import logo from '../../../public/logomain_smaller.png'
-import Cart from './Cart';
-import classes from './Midbar.module.scss';
+import { Row, Col } from "antd";
+import Image from "next/image";
+import router from "next/router";
+import logo from "../../../public/logomain_smaller.png";
+import Cart from "./Cart";
+import classes from "./MidBar.module.scss";
 
 const MidBar = () => {
-    const freeShipping = 120;
-  
+  const freeShipping = 120;
+
   return (
     <>
-        <Row justify="space-between">
-            <Col span={16}>
-                <Row justify="start">
-                    <Col>
-                       <Image src={logo} alt={'Logo'} quality={100} onClick={() => router.push("/")} className={classes.image}/>
-                    </Col>
-                </Row>
+      <Row justify="space-between">
+        <Col span={16}>
+          <Row justify="start">
+            <Col>
+              <Image
+                src={logo}
+                alt={"Logo"}
+                quality={100}
+                onClick={() => router.push("/")}
+                className={classes.image}
+              />
             </Col>
-            <Col span={8}>
-                <Row justify="end">
-                    <Col>
-                       <Cart/>
-                    </Col>
-                </Row>
+          </Row>
+        </Col>
+        <Col span={8}>
+          <Row justify="end">
+            <Col>
+              <Cart />
             </Col>
-        </Row>
+          </Row>
+        </Col>
+      </Row>
     </>
   );
 };
